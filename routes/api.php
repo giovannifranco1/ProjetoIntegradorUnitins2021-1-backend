f0bd31aee346571ae72cfda3d80e7592efc6103a
giovannifranco1 <55842499+giovannifranco1@users.noreply.github.com>
2021-05-07 22:12:38 -0300

    leaving jwt public

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
 Route::post('tecnico/store' , 'App\Http\Controllers\TecnicoController@store');
 Route::post('cooperado/store' , 'App\Http\Controllers\CooperadoController@store');
 Route::get('painel' , 'App\Http\Controllers\PainelController@index');
