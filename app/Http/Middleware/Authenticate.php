4adbd3ed54fb74b50ef4cb4a4fc7a65f6eec10f2
giovannifranco1 <55842499+giovannifranco1@users.noreply.github.com>
2021-05-07 23:12:14 -0300

    Middleware redirect correction

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return response()->json('not authorized' , 401);
+            return response()->json(['message' => 'not authorized' , 401]);
         }
     }
 }
