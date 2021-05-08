23f59190c19a8941710291a670f8d963efea40d5
giovannifranco1 <55842499+giovannifranco1@users.noreply.github.com>
2021-05-08 15:35:51 -0300

    install spadie

--- app/Models/Tecnico.php
+++ app/Models/Tecnico.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
 
 class Tecnico extends Model
 {
-    protected $fillable = ['numero_registro', 'senha', 'id_pessoa', 'id_grupo'];
+    use HasRoles;
+
+    protected $fillable = [
+        'numero_registro',
+        'senha', 'id_pessoa',
+        'id_grupo'
+    ];
+
     protected $table = 'tecnico';
 }
--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
-
     ],
 
     /*
