7db18c32ba0a8194602cb589b4468095e1e714ee
giovannifranco1 <55842499+giovannifranco1@users.noreply.github.com>
2021-05-08 23:08:18 -0300

    implementação select PainelController

--- app/Http/Controllers/PainelController.php
+++ app/Http/Controllers/PainelController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tecnico;
+use App\Models\Visita;
 use Illuminate\Http\Request;
 
 class PainelController extends Controller
 {
-    public function index()
-    {
+    public function index(Request $request){
+        $tecnico = Tecnico::where('nome' , $request->tecnico)->first();
+        $visita = Visita::where('id_tecnico', 'like', "%{$tecnico->id}%")
+        ->orderBy('dia_visita' , 'desc')
+        ->paginate(7);
 
+        return response()->json(compact('visita'));
     }
 }
--- database/migrations/2021_05_08_152647_create_permission_tables.php
+++ database/migrations/2021_05_08_152647_create_permission_tables.php
@@ -24,8 +24,6 @@ class CreatePermissionTables extends Migration
             $table->bigIncrements('id');
             $table->string('name');       // For MySQL 8.0 use string('name', 125);
             $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
-            $table->timestamps();
-
             $table->unique(['name', 'guard_name']);
         });
 
@@ -34,7 +32,6 @@ class CreatePermissionTables extends Migration
             $table->string('name');       // For MySQL 8.0 use string('name', 125);
             $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
             $table->timestamps();
-
             $table->unique(['name', 'guard_name']);
         });
 
