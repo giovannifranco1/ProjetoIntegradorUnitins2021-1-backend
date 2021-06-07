0a72f656558233917dac9945cd4aac7d53f90aba
giovannifranco1 <55842499+giovannifranco1@users.noreply.github.com>
2021-06-07 01:15:13 -0300

    correção rota visita

--- app/Models/Visita.php
+++ app/Models/Visita.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Visita extends Model
 {
-  use HasFactory;
   protected $fillable = [
     'horario_estimado_visita',
     'id_propriedade',
